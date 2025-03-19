44dcc336bf608b124c05099faf40ac58512e5452
Gaku Takahashi <gakut2002@gmail.com>
2025-03-20 08:01:43 +0900

    set drawer

--- src/components/ThreadDetails.tsx
+++ src/components/ThreadDetails.tsx
@@ -1,5 +1,6 @@
 import LinkedThreadDetails from "./LinkedThreadDetails"
 import { Thread } from '@/typeDeclar/typeComp';
+import FormThreads from '@/components/FormThreads';
 
 type ThreadsProp = {
     threads: Thread[];
@@ -8,6 +9,7 @@ type ThreadsProp = {
 export default function ThreadDetails({threads}: ThreadsProp){
     return (
     <>
+    <FormThreads />
     {threads.forEach((t: Thread) => (
         <LinkedThreadDetails thread={t} key={t.id} />
     ))}
