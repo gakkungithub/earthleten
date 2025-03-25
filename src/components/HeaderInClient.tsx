d16eb1aad513ba60602c84ec9dbd186c0a56e33f
Gaku Takahashi <gakut2002@gmail.com>
2025-03-25 21:35:34 +0900

    enabled changing color of link buttons and removing buttons of current page

--- src/components/HeaderInClient.tsx
+++ src/components/HeaderInClient.tsx
@@ -14,24 +14,30 @@ export default function HeaderInClient() {
             Earthlete
         </h1>
         <ul className="flex bg-blue-600 mb-4 pl-2">
-        <li className="block px-4 py-2 my-1 hover:bg-gray-100 rounded">
+        <li className={`block px-4 py-2 my-1 hover:bg-gray-100 rounded ${pathname === '/' ? "bg-fuchsia-600" : ""}`}>
             <Link className="no-underline text-blue-300" href="/">
             ホーム</Link></li>
-        <li className="block text-blue-300 px-4 py-2 my-1 hover:bg-gray-100 rounded">
+        <li className={`block text-blue-300 px-4 py-2 my-1 hover:bg-gray-100 rounded ${pathname === '/threads' ? "bg-fuchsia-600" : ""}`}>
             <Link className="no-underline text-blue-300" href="/threads">
             掲示板</Link></li>
         </ul>
         <ul className="flex fixed top-3 right-2">
+        {pathname !== '/logIn' &&
         <li className="block px-3 py-1 my-1 hover:bg-gray-100 rounded">
             <Link className="no-underline text-blue-600" href="/logIn">
             ログイン</Link></li>
+        }
+        {pathname !== '/addAcount' &&
         <li className="block px-3 py-1 my-1 hover:bg-gray-100 rounded">
             <Link className="no-underline text-orange-400" href="/addAcount">
             新規登録</Link></li>
+        }
+        {pathname !== '/editProfile' &&
         <li className="px-1 py-1 mx-2 rounded-full border">
             <Link className="text-blue-300" href="/editProfile">
             <Image src='/defaultIcon.png' alt="" width={32} height={32}/>
             </Link></li>
+        }
         </ul>
         </>
     );
