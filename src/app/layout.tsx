21a32579674bb84c7214c20635c0afb6767947c5
Gaku Takahashi <gakut2002@gmail.com>
2025-03-30 03:49:10 +0900

    enabled setting metadata in server side

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,112 +1,60 @@
-'use client';
-
 import './globals.css';
+
+import { Metadata } from 'next';
+
 // googleのフォントの一種であるInconsolataをインポートしている
 import { Inconsolata } from 'next/font/google';
 
-import Link from 'next/link';
-import Image from 'next/image';
+import { headers } from 'next/headers';
 
-import { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
-
-import UseAuth from '@/lib/useAuth';
+import HeaderInClient from '@/components/HeaderInClient';
 
 const fnt = Inconsolata({ subsets: ['latin']});
 
 export default function RootLayout({children,}: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-  const loginUser = UseAuth(pathname);
-
-  const [showLogoutMenu, setShowLogoutMenu] = useState<boolean>(false);
-  
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    setShowLogoutMenu(false);
-  };
-
-  useEffect(() => {
-    document.title = (() : string => {
-      switch (pathname) {
-        case '/':
-          return 'ホームページ - Earthlete';
-        case '/threads':
-          return 'スレッド一覧 - Earthlete';
-        case '/logIn':
-          return 'ログイン - Earthlete';
-        case '/logOut':
-          return 'ログアウト - Earthlete';
-        case '/addAccount':
-          return 'アカウント登録 - Earthlete';
-        case '/editProfile':
-          return 'プロフィール設定 - Earthlete';
-        default:
-          return 'Earthlete';
-      }
-    })();
-    setShowLogoutMenu(false);
-    const metaDescription = document.querySelector('meta[name="description"]');
-    if (metaDescription) {
-      metaDescription.setAttribute('content', 'The App for athletes');
-    }
-  }, [pathname])
-
   return (
     <html lang="ja">
       <body className={fnt.className}>
-        <h1 className="text-4xl text-indigo-800 font-bold my-2">
-            Earthlete
-        </h1>
-        <ul className="flex bg-blue-600 mb-4 pl-2">
-        <li className={`block px-4 py-2 my-1 hover:bg-gray-100 rounded ${pathname === '/' ? "bg-fuchsia-600" : ""}`}>
-            <Link className="no-underline text-blue-300" href="/">
-            ホーム</Link></li>
-        <li className={`block text-blue-300 px-4 py-2 my-1 hover:bg-gray-100 rounded ${pathname === '/threads' ? "bg-fuchsia-600" : ""}`}>
-            <Link className="no-underline text-blue-300" href="/threads">
-            掲示板</Link></li>
-        </ul>
-        <ul className="flex fixed top-3 right-2">
-        {pathname !== '/logIn' && !loginUser &&
-        <li className="block px-3 py-1 my-1 hover:bg-gray-100 rounded">
-            <Link className="no-underline text-blue-600" href="/logIn">
-            ログイン</Link></li>
-        }
-        {pathname !== '/logOut' && loginUser &&
-        <li className="block px-3 py-1 my-1 hover:bg-gray-100 rounded">
-            <button className="text-blue-600" onClick={() => setShowLogoutMenu(!showLogoutMenu)}>
-              ログアウト</button></li>
-        }
-        {pathname !== '/addAccount' && !loginUser &&
-        <li className="block px-3 py-1 my-1 hover:bg-gray-100 rounded">
-            <Link className="no-underline text-orange-400" href="/addAccount">
-            新規登録</Link></li>
-        }
-        {pathname !== '/editProfile' && loginUser &&
-        <li className="px-1 py-1 mx-2 rounded-full border">
-            <Link className="text-blue-300" href="/editProfile">
-            <Image src='/defaultIcon.png' alt="" width={32} height={32}/>
-            </Link></li>
-        }
-        </ul>
-        {showLogoutMenu && loginUser && 
-        <div className="fixed flex top-1/3 justify-center text-center w-full h-full z-20">
-          <div className=" border-black border-2 w-3/5 md:w-2/5 lg:w-1/5 h-1/5">
-            <p className="my-6">ログアウトしますか？</p>
-            <ul className="flex justify-around">
-              <li><Link className="no-underline text-white bg-blue-600 p-4 rounded" onClick={ () => setShowLogoutMenu(false) } href={pathname}>
-              キャンセル</Link></li>
-              <li><Link className="no-underline text-white bg-red-600 p-4 rounded" onClick={handleLogout} href="/logOut">
-              ログアウト</Link></li>
-            </ul>
-          </div>
-        </div>
-        }
+        <HeaderInClient />
         <div className="mx-2">
           {children}
         </div>
       </body>
     </html>
   );
+}
+
+export async function generateMetadata(): Promise<Metadata>{
+  let title = '';
+
+  const pathname = (await headers()).get("x-url");
+  switch (pathname) {
+    case '/':
+      title = 'ホームページ - Earthlete';
+      break;
+    case '/threads':
+      title = 'スレッド一覧 - Earthlete';
+      break;
+    case '/logIn':
+      title = 'ログイン - Earthlete';
+      break;
+    case '/logOut':
+      title = 'ログアウト - Earthlete';
+      break;
+    case '/addAccount':
+      title = 'アカウント登録 - Earthlete';
+      break;
+    case '/editProfile':
+      title = 'プロフィール設定 - Earthlete';
+      break;
+    default:
+      title = 'Earthlete';
+  }
+
+  return {
+    title: title,
+    description: 'The app for athletes'
+  };
 }
---
+++ src/middleware.tsx
@@ -0,0 +1,23 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    const requestUrl = request.url;
+    if (requestUrl && requestUrl.split('/').length > 1) {
+        const currentpath = `/${requestUrl.split('/').pop()}` || '';
+        requestHeaders.set("x-url", currentpath);
+        return NextResponse.next({
+            request: {
+                headers: requestHeaders
+            }
+        });
+    }
+    else {
+        requestHeaders.set("x-url", '');
+        return NextResponse.next({
+            request: {
+                headers: requestHeaders
+            }
+        });
+    }
+}
