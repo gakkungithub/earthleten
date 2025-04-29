cead2b14a16dff14cd1f33184cc002ef219cd0ee
Gaku Takahashi <gakut2002@gmail.com>
2025-04-29 23:54:04 +0900

    added awards tables show function

--- src/app/playercoach/profile/[id]/page.tsx
+++ src/app/playercoach/profile/[id]/page.tsx
@@ -13,10 +13,14 @@ type Result = {
     rows: (string | number | null)[][];
 };
 
+type Award = {
+    section: string;
+    titles: {name: string; years: number[]}[];
+}
   
 export default async function PlayerCoachProfilePage({params}: {params: {id: string}}) {
-    const jsonData = fs.readFileSync('./public/jsonfile/sports_kgavvaaxha_1.json', 'utf-8');
-    const {scripts, results}: {scripts: Script[], results: Result[]} = JSON.parse(jsonData);
+    const jsonData = fs.readFileSync('./public/jsonfile/sports_kgavvaaxha_2.json', 'utf-8');
+    const {scripts, results, awards}: {scripts: Script[], results: Result[], awards: Award[]} = JSON.parse(jsonData);
 
     return (
         <>
@@ -43,56 +47,83 @@ export default async function PlayerCoachProfilePage({params}: {params: {id: str
                 </ul>
             </div>
         </div>
-        <div className="w-full h-128 overflow-y-auto border-2 p-2 my-2">
+        <div className="w-full h-128 overflow-y-auto border-2 p-2 my-2 rounded">
             {scripts.map((script, scriptIndex) => (
                 <div key={scriptIndex}>
-                    <p className="border-b-2">{script.section}</p>
+                    <p className="border-b-2 font-bold">{script.section}</p>
                     {script.texts.map((text, textIndex) => (
                         <p key={textIndex}>{text}</p>
                     ))}
                 </div>
             ))}
         </div>
+        <h2 className="font-bold text-3xl my-2">- 成績 -</h2>
+        <div className="h-128 overflow-y-auto border-2 p-2">
             {results.map((result, recordIndex) => (
             <div key={recordIndex} className="overflow-x-auto">
                 <table className="table-auto w-full border-collapse border border-gray-300">
+                    <caption className="caption-top text-left font-semibold text-lg mb-2">
+                        {result.position}
+                    </caption>
+                    <thead className="bg-gray-200">
+                        <tr>
+                            {result.columns.map((col, colIndex) => (
+                                <th key={colIndex} className="border p-2 whitespace-nowrap">
+                                    {col}
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {result.rows.map((row, index) => (
+                            <tr key={index} className="text-center">
+                                {row.map((cell, cellIndex) => (
+                                    <td key={cellIndex} className="border px-4 py-2 whitespace-nowrap">
+                                        {cell !== null ? cell : "-"}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            ))}
+        </div>
+        <h2 className="font-bold text-3xl my-2">- 受賞 -</h2>
+        <div className="h-128 overflow-y-auto border-2 p-2">
+            {awards.map((award, awardIndex) => (
+            <div key={awardIndex} className="overflow-x-auto">
+                <table className="table-auto w-full border-collapse border border-gray-300">
                 <caption className="caption-top text-left font-semibold text-lg mb-2">
-                    {result.position}
+                    {award.section}
                 </caption>
                 <thead className="bg-gray-200">
                     <tr>
-                        {result.columns.map((col, colIndex) => (
-                            <th key={colIndex} className="border p-2">
-                                {col}
-                            </th>
-                        ))}
+                        <th className="border p-2 whitespace-nowrap">
+                            タイトル名 
+                        </th>
+                        <th className="border p-2 whitespace-nowrap">
+                            年度
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
-                    {result.rows.map((row, index) => (
-                        <tr key={index} className="text-center">
-                            {row.map((cell, cellIndex) => (
-                                <td key={cellIndex} className="border px-4 py-2">
-                                    {cell !== null ? cell : "-"}
-                                </td>
-                            ))}
+                    {award.titles.map((title, titleIndex) => (
+                        <tr key={titleIndex} className="text-center">
+                            <td className="border px-4 py-2 whitespace-nowrap">
+                                {title.name}
+                            </td>
+                            <td className="border px-4 py-2 whitespace-nowrap">
+                                {title.years.length != 1 && `(${title.years.length}回) `}
+                                {title.years.join(', ')}
+                            </td>               
                         </tr>
                     ))}
                 </tbody>
                 </table>
             </div>
             ))}
+        </div>
         </>
     )
 }
-
-// {
-//     "year": 2015,
-//     "team": "大阪ホワイトタイガース",
-//     "stats": {
-//         "試合数": 25,
-//         "勝利": 15,
-//         "敗北": 5,
-//         "防御率": 2.30
-//       }
-// },
